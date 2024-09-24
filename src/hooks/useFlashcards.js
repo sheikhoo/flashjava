269cc02de89ce8d3f0ce746e5625c430d86742cd
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2024-09-24 18:35:18 +0330

    Update useFlashcards.js

--- src/hooks/useFlashcards.js
+++ src/hooks/useFlashcards.js
@@ -11,7 +11,17 @@ const INITIAL_FLASHCARDS = [
     { id: 8, front: "What does the “jlink” command do?", back: "The jlink command creates the executable that contains the required pieces to run the program but not compiling." },
     { id: 9, front: "What are (APIs) Application Programming Interfaces?", back: "APIs are common pieces of functionality and algorithms that developers need like: -Stringbuilder -sort in collections" },
     { id: 10, front: "What does IDE(Integrated Development Environment) do?", back: "It makes writing and running the code easier. common java IDEs: -Eclips -Intellij -NetBeans" },
-    
+
+    { id: 11, front: "How often does the version number of java get incremented?", back: "Every six month" },
+    { id: 12, front: "How often does Oracle release a LTS (Long Term Support)?", back: "Every 3 years" },
+    { id: 13, front: "What is the alternative for Oracle's JDK?", back: "Open-JDK which is base on same source code." },
+    { id: 14, front: "What are the key benefits of java?", back: "1-Object Oriented 2-Encapsulation 3-Platform Independent 4-Robust 5-Simple 6-Secure 7-Multithreaded 8-Backward Compatibility" },
+    { id: 15, front: "What does object oriented mean?", back: "means all code is defined in classes and most of these classes can instantioted into object. java allows functional programming whithin a class." },
+    { id: 16, front: "What is a procedural language?", back: "It has routies or methods not classes." },
+    { id: 17, front: "What types of programming do we have?", back: "-Object Oriented -Procedural -Functional" },
+    { id: 18, front: "What do access modifires do?", back: "They protect data from unintended access and modification." },
+    { id: 19, front: "What is Encapsulation?", back: "Java support access modifires to protect data from unintended access and modification." },
+    { id: 20, front: "Is Encapsulation an aspect of Object Oriented?", back: "Most people cosider it to be." },
 ];
 
 const FLASHCARDS_KEY = 'flashcards';
